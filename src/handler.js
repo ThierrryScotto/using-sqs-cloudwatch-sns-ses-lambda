2b94ab834567c7208da7ad34456eb7612a01a62f
ThierrryScotto <thierry_192@outlook.com>
2021-01-03 01:06:32 -0300

    some adjustments

--- src/handler.js
+++ src/handler.js
@@ -10,19 +10,15 @@ module.exports.sendEmail = async (event) => {
 
   const messages = await sqs.receiveMessage();
 
-  for await(let message of messages) {
-    await Message.create({ messageId: message.MessageId, messageBody: message.Body});
-    await sqs.deleteMessage(message);
+  if (messages.length > 0) {
+    for await(let message of messages) {
+      await Message.create({ messageId: message.MessageId, messageBody: message.Body});
+      await sqs.deleteMessage(message);
+    }
+  } else {
+    return { statusCode: 200, body: JSON.stringify({ message: 'No message', }, null, 2),
+    };
   }
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(
-      {
-        message: 'messages saved',
-      },
-      null,
-      2
-    ),
-  };
+  return { statusCode: 201, body: JSON.stringify({ message: 'messages saved', }, null, 2), };
 };
