f1269a597e4c8c40dd462b52c8f8fcc6fbff6d1d
ThierrryScotto <thierry_192@outlook.com>
2021-01-04 22:39:48 -0300

    added logs

--- src/handler.js
+++ src/handler.js
@@ -7,17 +7,18 @@ const sqs = require('./services/sqs');
 const Message = require('./models/message')
 
 module.exports.sendEmail = async (event) => {
-
+  console.log('-- Function sendmail --');
   const messages = await sqs.receiveMessage();
 
   if (messages.length > 0) {
     for await(let message of messages) {
       await Message.create({ messageId: message.MessageId, messageBody: message.Body});
-      await sqs.deleteMessage(message);
+      // await sqs.deleteMessage(message);
     }
+    console.log('-- Success the get and delete messages --');
   } else {
-    return { statusCode: 200, body: JSON.stringify({ message: 'No message', }, null, 2),
-    };
+    console.log('-- Empty Queue --');
+    return { statusCode: 200, body: JSON.stringify({ message: 'No message', }, null, 2), };
   }
 
   return { statusCode: 201, body: JSON.stringify({ message: 'messages saved', }, null, 2), };
